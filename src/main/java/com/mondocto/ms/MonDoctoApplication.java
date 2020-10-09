02e32891f1b67f79c5cedb13972c3d618f91b793
Othmane Bentaleb <othmanebentaleb90@gmail.com>
2020-10-09 14:03:05 +0200

    création de la classe principale

---
+++ src/main/java/com/mondocto/ms/MonDoctoApplication.java
@@ -0,0 +1,13 @@
+package com.mondocto.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonDoctoApplication{
+
+    public static void main(String[]args){
+        SpringApplication.run(MonDoctoApplication.class, args);
+    }
+}
+
